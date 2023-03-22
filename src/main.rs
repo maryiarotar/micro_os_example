38b98b1d057942a75ac1110ffece8e554730dd8d
Maryia Rotar <rotarmaryia@gmail.com>
2023-03-22 22:01:02 +0200

    minimal kernel

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
+//to build target for triple x86-64-unknown-none 
+
 #![no_std]
 #![no_main]
 // #![feature(start)]
 //extern crate libc;
 use core::panic::PanicInfo;
 
+
 #[panic_handler]
 fn panic(_panic: &PanicInfo) -> !{
     loop {}
 }
 
+
+static HELLO: &[u8] = b"Mini Rust Os!";
+
+//entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+
+    let vga_buffer: *mut u8 = 0xb8000 as *mut u8; //raw pointer to vga buffer
+
+    for (i, &byte) in HELLO.iter().enumerate(){
+
+        unsafe { 
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+
+    }
+
     loop {}
 }
+
+
+
+
+
+
+
 
 //for creating a minimal binary that runs on top of an existing operating system, including libc:
 //<unstable feature>
@@ -20,19 +46,3 @@ pub extern "C" fn _start() -> ! {
 // fn start(_argc: isize, _argv: *const *const u8) -> isize {
 //     0
 // }
-
-/*
-//for compilation without host OS:
-//rustup target add thumbv7em-none-eabihf
-//cargo build --target thumbv7em-none-eabihf
-
-----or add this to .cargo/config.toml
-[target.'cfg(target_os = "linux")']
-rustflags = ["-C", "link-arg=-nostartfiles"]
-
-[target.'cfg(target_os = "windows")']
-rustflags = ["-C", "link-args=/ENTRY:_start /SUBSYSTEM:console"]
-
-[target.'cfg(target_os = "macos")']
-rustflags = ["-C", "link-args=-e __start -static -nostartfiles"]
-*/
